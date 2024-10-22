1416d34c0facca8f9b5cfad7c57b7f45640ed7fc
Alwalaswarnaraj <allwalaswarnaraj541@gmail.com>
2024-10-22 18:32:38 +0530

    book updated and added getters and setters

--- Book.java
+++ Book.java
@@ -6,4 +6,46 @@ public class Book {
 	private String author;
 	private String publisher;
 	private Double price;
+	public int getBookId() {
+		return bookId;
+	}
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public Book() {}
+	public Book(int bookId, String bookName, String author, String publisher, Double price) {
+		super();
+		this.bookId = bookId;
+		this.bookName = bookName;
+		this.author = author;
+		this.publisher = publisher;
+		this.price = price;
+	}
+	
+	
 }
